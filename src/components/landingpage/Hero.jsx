28bb6c26f32f3b4cd54e1752df4d0b0892699b1f
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-05-04 19:01:16 +0300

    Create hero section responsive

--- src/components/landingpage/Hero.jsx
+++ src/components/landingpage/Hero.jsx
@@ -5,12 +5,29 @@ function Hero() {
   return (
     <section className="hero">
       <div className="text-container">
-        <h1>Profesyonel Bir CV Oluşturmak Artık Çok Kolay!</h1>
+        <h1>Profesyonel bir CV oluşturmak artık çok kolay!</h1>
         <p>Kullanımı kolay arayüzümüz ve özenle hazırlanmış şablonlarımız sayesinde dakikalar içinde profesyonel bir CV oluşturabilirsin. 🚀</p>
         <Link className="button primary__button" to="/templates"><img src="../../../public/assets/arrow.svg" alt="arrow" />CV Oluşturmaya Başla! ✨</Link>
+        <div className="customer__container">
+          <img src="../../../public/assets/customersprofilepictures.png" />
+          <p>3.1 milyon kullanıcı tarafından seviliyor!</p>
+        </div>
       </div>
       <div className="image-container">
         <img src="../../../public/assets/herocvimage.webp" alt="cv template photo" />
+        <a href="" className="imglink_1">
+          <div className="text-content">
+            <h4>Tahaberk Soysal</h4>
+            <p>Front-end Geliştirici</p>
+            <img src="../../../public/assets/fivestars.png" />
+          </div>
+          <div className="img-content">
+            <img src="../../../public/assets/profileletter.png" />
+          </div>
+        </a>
+        <a href="">
+
+        </a>
       </div>
     </section>
   )
