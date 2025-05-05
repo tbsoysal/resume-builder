fdab3637ebdd487a08a4e8fe59718e5e7bf13695
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-05-05 22:15:24 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/resume-builder/'
-})
+  base: mode === 'production' ? '/resume-builder/' : '/',
+}))
