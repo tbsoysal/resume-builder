1257b9346cc60254b8be0f207eb958b930fd49bb
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-05-20 00:37:50 +0300

    create scroll buttons

--- src/components/landingpage/Testimonials.jsx
+++ src/components/landingpage/Testimonials.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "../../styles/landingpage/testimonials.scss";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 function Testimonials() {
+  const carouselRef = useRef(null);
+  const [translateX, setTranslateX] = useState(0);
+  const [maxScroll, setMaxScroll] = useState(0);
+  console.log('translateX: ' + translateX)
+  console.log('maxScroll: ' + maxScroll)
+  useEffect(() => {
+    const ul = carouselRef.current;
+    if (ul) {
+      const containerWidth = ul.parentElement.offsetWidth;
+      const contentWidth = ul.scrollWidth;
+      const maxScroll = contentWidth - containerWidth;
+      setMaxScroll(-maxScroll - 32);
+    }
+  }, []);
   return (
     <section className="testimonials">
       <div>
         <h3>Binlerce Kişi Bizi Tercih Etti</h3>
         <div className="controlls">
-          <a className="passive"><svg className="passive" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" aria-label="Arrow Left"><path fill="currentColor" fill-rule="evenodd" d="M9.145 32.552L31.772 9.925l3.771 3.771-16.19 16.19h33.942v5.333H19.353l16.19 16.19-3.771 3.77L9.145 32.553z" clip-rule="evenodd"></path></svg></a>
-          <a><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" ><path fill="currentColor" fill-rule="evenodd" d="M54.853 31.449L32.227 54.076l-3.771-3.771 16.19-16.19H10.702v-5.333h33.942l-16.19-16.19 3.771-3.77 22.627 22.627z" clip-rule="evenodd"></path></svg></a>
+          <a onClick={() => setTranslateX(translateX + 633)} className={`${translateX >= 0 ? "passive" : ""}`} ><svg className={`${translateX >= 0 ? "passive" : ""}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" aria-label="Arrow Left"><path fill="currentColor" fillRule="evenodd" d="M8.145 32.552L31.772 9.925l3.771 3.771-16.19 16.19h33.942v5.333H19.353l16.19 16.19-3.771 3.77L9.145 32.553z" clipRule="evenodd"></path></svg></a>
+          <a onClick={() => setTranslateX(translateX - 633)} className={`${translateX <= maxScroll ? "passive" : ""}`}><svg className={`${translateX <= maxScroll ? "passive" : ""}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" ><path fill="currentColor" fillRule="evenodd" d="M54.853 31.449L32.227 54.076l-3.771-3.771 16.19-16.19H10.702v-5.333h33.942l-16.19-16.19 3.771-3.77 22.627 22.627z" clipRule="evenodd"></path></svg></a>
         </div>
       </div>
-      <ul>
+      <ul ref={carouselRef} style={{ transform: `translateX(${Math.max(translateX, maxScroll)}px)` }}>
         <li>
           <p>HazırCV kariyerimi değiştirdi. Sıkışıp kaldığım, ilerleme imkanı olmayan bir fast food işindeydim ve kendi hazırladığım özgeçmişi kullanıyordum. HazırCV’yi Reddit’te keşfettim. Diğer “ücretsiz” araçlar gibi özgeçmişinizi elinizde tutmak yerine, HazırCV gerçekten işe yarayan bir hizmet sundu. HazırCV sayesinde FedEx Ground ve The Source’ta iş buldum.</p>
           <div>
@@ -18,7 +35,7 @@ function Testimonials() {
               <h4>Kerem Yılmaz</h4>
               <p>İnsan Kaynakları & Pazarlama</p>
             </div>
-            <img src="/public/assets/reddit.svg" alt="reddit logo" />
+            <img src="/assets/reddit.svg" alt="reddit logo" />
           </div>
         </li>
         <li>
@@ -28,7 +45,7 @@ function Testimonials() {
               <h4>@ZFYildiz</h4>
               <p>Yazılım Geliştirici</p>
             </div>
-            <img src="/public/assets/pletter.svg" alt="profile logo" />
+            <img src="/assets/pletter.svg" alt="profile logo" />
           </div>
         </li>
         <li>
@@ -52,7 +69,7 @@ function Testimonials() {
               <h4>Utku Çağlar Yazıcı</h4>
               <p>Pazarlama</p>
             </div>
-            <img src="/public/assets/pletter.svg" alt="profile logo" />
+            <img src="/assets/pletter.svg" alt="profile logo" />
           </div>
         </li>
         <li>
@@ -68,7 +85,7 @@ function Testimonials() {
           </div>
         </li>
       </ul>
-    </section>
+    </section >
   )
 }
 
