66f5436bbb8278ac06ba3df2edb6c3a8337e7a98
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-07-04 18:22:18 +0300

    change image paths

--- src/pages/TemplatesPage.jsx
+++ src/pages/TemplatesPage.jsx
@@ -10,11 +10,11 @@ function TemplatesPage() {
       <main className='templates'>
         <h2>Hazır CV Şablonları</h2>
         <div class="grid-container">
-          <a href=""><img src="../../public/assets/templateimages/cvtemplateimage01.webp" alt="cvtemplate01" /></a>
-          <a href=""><img src="../../public/assets/templateimages/cvtemplateimage02.webp" alt="cvtemplate02" /></a>
-          <a href=""><img src="../../public/assets/templateimages/cvtemplateimage03.webp" alt="cvtemplate03" /></a>
-          <a href=""><img src="../../public/assets/templateimages/cvtemplateimage04.webp" alt="cvtemplate04" /></a>
-          <a href=""><img src="../../public/assets/templateimages/cvtemplateimage05.webp" alt="cvtemplate05" /></a>
+          <a href=""><img src={`${import.meta.env.BASE_URL}assets/cvtemplateimage01.webp`} alt="cvtemplate01" /></a>
+          <a href=""><img src={`${import.meta.env.BASE_URL}assets/cvtemplateimage02.webp`} alt="cvtemplate02" /></a>
+          <a href=""><img src={`${import.meta.env.BASE_URL}assets/cvtemplateimage03.webp`} alt="cvtemplate03" /></a>
+          <a href=""><img src={`${import.meta.env.BASE_URL}assets/cvtemplateimage04.webp`} alt="cvtemplate04" /></a>
+          <a href=""><img src={`${import.meta.env.BASE_URL}assets/cvtemplateimage05.webp`} alt="cvtemplate05" /></a>
         </div>
       </main>
       <Footer />
