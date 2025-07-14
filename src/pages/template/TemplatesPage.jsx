a2fbb41368ac7b708cd57b9c90e6f3515827c03e
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-07-14 16:52:09 +0300

    Link from templates page

--- src/pages/template/TemplatesPage.jsx
+++ src/pages/template/TemplatesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '@/components/common/Navbar.jsx';
 import Footer from '@/components/common/Footer.jsx';
 import styles from './TemplatesPage.module.scss';
+import { Link } from 'react-router-dom';
 
 function TemplatesPage() {
   return (
@@ -10,7 +11,7 @@ function TemplatesPage() {
       <main className={styles.templates}>
         <h2>Hazır CV Şablonları</h2>
         <div className={styles.gridContainer}>
-          <a href=""><img src={`${import.meta.env.BASE_URL}assets/templateimages/cvtemplateimage01.webp`} alt="cvtemplate01" /></a>
+          <Link to='/create'><img src={`${import.meta.env.BASE_URL}assets/templateimages/cvtemplateimage01.webp`} alt="cvtemplate01" /></Link>
           <a href=""><img src={`${import.meta.env.BASE_URL}assets/templateimages/cvtemplateimage02.webp`} alt="cvtemplate02" /></a>
           <a href=""><img src={`${import.meta.env.BASE_URL}assets/templateimages/cvtemplateimage03.webp`} alt="cvtemplate03" /></a>
           <a href=""><img src={`${import.meta.env.BASE_URL}assets/templateimages/cvtemplateimage04.webp`} alt="cvtemplate04" /></a>
