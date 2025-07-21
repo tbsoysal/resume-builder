42ad1e5ef52de66f02ed6c9298b7843fdc9519fa
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-07-21 20:52:27 +0300

    Fix pdf issues

--- src/components/templates/template01/CvTemplate01BlobStyles.jsx
+++ src/components/templates/template01/CvTemplate01BlobStyles.jsx
@@ -9,64 +9,64 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 400,
     height: '100%',
-    width: '100%',
+    width: '100%'
   },
   leftColumn: {
     width: '45%',
     backgroundColor: '#1b3142',
-    padding: 28,
+    padding: 21,
     color: 'white',
   },
   rightColumn: {
     flex: 1,
-    padding: 28,
+    padding: 21,
   },
   firstName: {
-    fontSize: 35,
+    fontSize: 26.25,
     fontWeight: 700,
   },
   jobTitle: {
-    fontSize: 20, // 1.5em
-    marginBottom: 15,
+    fontSize: 19.5,
+    fontWeight: 400,
+    marginBottom: 15
   },
   contactListItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 5,
+    marginBottom: 7.5,
   },
   contactListText: {
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: 300,
-    marginLeft: 5,
+    marginLeft: 3.75,
   },
   section: {
     display: 'block',
-    marginBottom: 20,
+    marginBottom: 15,
   },
   sectionHeading: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     gap: 5,
-    fontSize: 16,
+    fontSize: 15,
     fontWeight: 600,
     textAlign: 'center',
-    letterSpacing: 1,
+    letterSpacing: 0.75,
     backgroundColor: 'rgba(255,255,255,0.1)',
-    borderRadius: 2,
-    marginBottom: 10,
-    paddingVertical: 3,
+    borderRadius: 1.5,
+    marginBottom: 7.5,
+    paddingVertical: 2.25,
     paddingHorizontal: 0,
     textTransform: 'uppercase',
   },
   sectionIcon: {
-    maxWidth: 18,
-    maxHeight: 18
+    maxWidth: 12,
+    maxHeight: 12
   },
-
   profileText: {
     display: 'block',
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: '500',
     lineHeight: 1.4,
   },
@@ -78,85 +78,87 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 15,
+    marginBottom: 11.25,
   },
   languageName: {
     display: 'block',
-    fontSize: 15,
+    fontSize: 12,
     maxWidth: '65%'
   },
   levelContainer: {
     flexDirection: 'row',
-    gap: 5,
+    gap: 7.5,
   },
 
   documentsList: {
     flexDirection: 'column',
-    marginBottom: 15
+    marginBottom: 11.25
   },
   documentOrigin: {
-    fontWeight: 600,
-    fontSize: 15
+    fontWeight: 700,
+    fontSize: 12
   },
   documentDate: {
-    fontWeight: 500,
-    fontSize: 14
+    fontWeight: 600,
+    fontSize: 12
   },
   documentName: {
     fontWeight: 400,
-    fontSize: 14
+    fontSize: 12
   },
 
   rightSectionHeading: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 5,
+    gap: 3.75,
     backgroundColor: '#efefef',
-    padding: 5,
-    fontWeight: '600',
-    fontSize: 16,
+    padding: 3.75,
+    fontWeight: 600,
+    fontSize: 15,
     textAlign: 'center',
     textTransform: 'uppercase',
-    marginBottom: 10,
+    marginBottom: 7.5,
   },
 
   experienceItem: {
-    marginBottom: 10,
+    marginBottom: 7.5,
   },
   companyName: {
-    fontWeight: 500,
-    fontSize: 15
+    fontWeight: 600,
+    fontSize: 12
   },
   jobName: {
-    fontSize: 15
+    fontSize: 12,
+    fontWeight: 500
   },
   jobDate: {
-    fontSize: 14
+    fontSize: 10.5,
+    fontWeight: 300
   },
   jobExplanation: {
-    fontSize: 14,
+    fontSize: 10.5,
   },
 
   educationItem: {
-    marginBottom: 10,
+    marginBottom: 11.5,
   },
   educationProgram: {
-    fontSize: 15,
-    fontWeight: 500
+    fontSize: 12,
+    fontWeight: 600
   },
   educationUniversity: {
-    fontSize: 15
+    fontSize: 12,
+    fontWeight: 500
   },
   educationDate: {
-    fontSize: 14
+    fontSize: 11
   },
 
   skillItem: {
-    fontSize: 14,
-    marginBottom: 5
+    fontSize: 12,
+    marginBottom: 3.75
   }
-
 });
 
 export default styles;
