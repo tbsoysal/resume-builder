d1550934a5aba609efb98173ba1b3497735bbf6b
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-07-23 21:03:55 +0300

    fix toggles logic in pdfblob

--- src/components/templates/template01/CvTemplate01Blob.jsx
+++ src/components/templates/template01/CvTemplate01Blob.jsx
@@ -56,95 +56,113 @@ const CvTemplate01Blob = ({ data }) => (
         </View>
 
         {/* Profile Text Section */}
-        <View style={styles.section}>
-          <View style={styles.sectionHeading}>
-            <ProfileIcon styles={styles} />
-            <Text>PROFİL</Text>
+        {
+          data.profileText &&
+          <View style={styles.section}>
+            <View style={styles.sectionHeading}>
+              <ProfileIcon styles={styles} />
+              <Text>PROFİL</Text>
+            </View>
+            <Text style={styles.profileText}>{data.profileText}</Text>
           </View>
-          <Text style={styles.profileText}>{data.profileText}</Text>
-        </View>
+        }
 
         {/* Languages Section */}
-        <View style={styles.section}>
-          <View style={styles.sectionHeading}>
-            <LanguageIcon styles={styles} />
-            <Text>YABANCI DİL</Text>
-          </View>
-          {/* Languages level icons*/}
-          <View style={styles.languagesContainer}>
-            {data.languages.map((language, index) => (
-              <View key={index} style={styles.languageRow}>
-                <Text style={styles.languageName}>{language.name}</Text>
-                <View style={styles.levelContainer}>
-                  {Array.from({ length: 5 }).map((_, i) => (
-                    <LanguageLevelIcon key={i} filled={i < language.level} />
-                  ))}
+        {
+          data.languages[0] &&
+          <View style={styles.section}>
+            <View style={styles.sectionHeading}>
+              <LanguageIcon styles={styles} />
+              <Text>YABANCI DİL</Text>
+            </View>
+            {/* Languages level icons*/}
+            <View style={styles.languagesContainer}>
+              {data.languages.map((language, index) => (
+                <View key={index} style={styles.languageRow}>
+                  <Text style={styles.languageName}>{language.name}</Text>
+                  <View style={styles.levelContainer}>
+                    {Array.from({ length: 5 }).map((_, i) => (
+                      <LanguageLevelIcon key={i} filled={i < language.level} />
+                    ))}
+                  </View>
                 </View>
+              ))}
+            </View>
+          </View>
+        }
+
+        {/* Documents Section */}
+        {
+          data.documents[0] &&
+          <View style={styles.section}>
+            <View style={styles.sectionHeading}>
+              <DocumentIcon styles={styles} />
+              <Text>BELGELER</Text>
+            </View>
+            {data.documents.map((doc, index) => (
+              <View key={index} style={styles.documentsList}>
+                <Text style={styles.documentOrigin}>{doc.origin}</Text>
+                <Text style={styles.documentDate}>{doc.date}</Text>
+                <Text style={styles.documentName}>{doc.name}</Text>
               </View>
             ))}
           </View>
-        </View>
-
-        {/* Documents Section */}
-        <View style={styles.section}>
-          <View style={styles.sectionHeading}>
-            <DocumentIcon styles={styles} />
-            <Text>BELGELER</Text>
-          </View>
-          {data.documents.map((doc, index) => (
-            <View key={index} style={styles.documentsList}>
-              <Text style={styles.documentOrigin}>{doc.origin}</Text>
-              <Text style={styles.documentDate}>{doc.date}</Text>
-              <Text style={styles.documentName}>{doc.name}</Text>
-            </View>
-          ))}
-        </View>
+        }
       </View>
 
       {/* Right Column */}
       <View style={styles.rightColumn}>
 
         {/* Experience Section */}
-        <View style={styles.section}>
-          <View style={styles.rightSectionHeading}>
-            <WorkIcon styles={styles} />
-            <Text>DENEYİM</Text>
-          </View>
-          {data.experience.map((experience, index) => (
-            <View key={index} style={styles.experienceItem}>
-              <Text style={styles.companyName}>{experience.companyName}</Text>
-              <Text style={styles.jobName}>{experience.jobName}</Text>
-              <Text style={styles.jobDate}>{experience.jobDate}</Text>
-              <Text style={styles.jobExplanation}>{experience.jobExplanation}</Text>
+        {
+          data.experience[0] &&
+          <View style={styles.section}>
+            <View style={styles.rightSectionHeading}>
+              <WorkIcon styles={styles} />
+              <Text>DENEYİM</Text>
             </View>
-          ))}
-        </View>
+            {data.experience.map((experience, index) => (
+              <View key={index} style={styles.experienceItem}>
+                <Text style={styles.companyName}>{experience.companyName}</Text>
+                <Text style={styles.jobName}>{experience.jobName}</Text>
+                <Text style={styles.jobDate}>{experience.jobDate}</Text>
+                <Text style={styles.jobExplanation}>{experience.jobExplanation}</Text>
+              </View>
+            ))}
+          </View>
+        }
 
         {/* Education Section */}
-        <View style={styles.section}>
-          <View style={styles.rightSectionHeading}>
-            <EducationIcon styles={styles} />
-            <Text>EĞİTİM</Text>
-          </View>
-          {data.education.map((edu, index) => (
-            <View key={index} style={styles.educationItem}>
-              <Text style={styles.educationProgram}>{edu.program}</Text>
-              <Text style={styles.educationUniversity}>{edu.university}</Text>
-              <Text style={styles.educationDate}>{edu.date}</Text>
+        {
+          data.education[0] &&
+          <View style={styles.section}>
+            <View style={styles.rightSectionHeading}>
+              <EducationIcon styles={styles} />
+              <Text>EĞİTİM</Text>
             </View>
-          ))}
-        </View>
+            {data.education.map((edu, index) => (
+              <View key={index} style={styles.educationItem}>
+                <Text style={styles.educationProgram}>{edu.program}</Text>
+                <Text style={styles.educationUniversity}>{edu.university}</Text>
+                <Text style={styles.educationDate}>{edu.date}</Text>
+              </View>
+            ))}
+          </View>
+        }
 
         {/* Skills Section */}
-        <View style={styles.section}>
-          <View style={styles.rightSectionHeading}>
-            <SkillsIcon styles={styles} />
-            <Text>YETENEKLER</Text>
+        {
+          data.skills[0] &&
+          <View style={styles.section}>
+            <View style={styles.rightSectionHeading}>
+              <SkillsIcon styles={styles} />
+              <Text>YETENEKLER</Text>
+            </View>
+            {data.skills.map((skill, index) => (
+              <Text key={index} style={styles.skillItem}>• {skill}</Text>
+            ))}
           </View>
-          {data.skills.map((skill, index) => (
-            <Text key={index} style={styles.skillItem}>• {skill}</Text>
-          ))}
-        </View>
+        }
       </View>
     </Page>
   </Document >
